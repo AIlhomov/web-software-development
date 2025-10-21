cbf737d5bd0b3dcc63bd773f80ce26f06be868d2
AIlhomov <Ilhomovvahid@gmail.com>
2025-10-21 23:28:37 +0300

    material

--- material/client/src/routes/+page.svelte
+++ material/client/src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<h1>Welcome</h1>
-<p><a href="/todos">Go to Todos</a></p>
-
-<style>
-  h1 {
-    margin-bottom: 0.25rem;
-  }
-  a {
-    text-decoration: none;
-  }
-</style>
+<p>Welcome to the home page!</p>
