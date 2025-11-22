de20ccce37d7d12e1495d085b5d847e049457892
AIlhomov <Ilhomovvahid@gmail.com>
2025-11-22 19:27:34 +0200

    materials

---
+++ svelte-deployment-test/client/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let message = $state("");
+
+    const fetchMessage = async () => {
+        const response = await fetch(
+            "https://hello-world.aalto-web-software-development.deno.net/",
+        );
+        const data = await response.json();
+        message = data.message;
+    };
+</script>
+
+<h1>Deployment demo!</h1>
+
+<p>Message from server: {message}</p>
+
+<button onclick={fetchMessage}>Fetch Message</button>
